1d5ecb4ec7472e908f3567d42e1a551080bfdd67
Carlo s A. Guillen <caguillen@google.com>
2014-07-08 10:41:32 -0700

    refactor(ngHintEvent): add loop to decorate all ngEventDirectives

--- decorator.js
+++ decorator.js
@@ -1,38 +1,36 @@
 angular.module('ngHintEvent',[])
   .config(['$provide',function($provide){
-    $provide.decorator('ngClickDirective', function($delegate) {
-      var original = $delegate[0].compile;
-      var falseBinds = [];
-      $delegate[0].compile = function(element, attrs, transclude) {
-        return function ngEventHandler(scope, element, attrs) {
-          var ngEventDirs = eeLib.getEventDirectives(), boundFunc;
-          for(var attr in attrs.$attr) {
-            boundFunc = attrs[attr].substring(0,attrs[attr].indexOf('('));
-            if(ngEventDirs[attr] && !scope[boundFunc]) {
-              var id = (element[0].id) ? ' with id: #'+element[0].id : '';
-              var type = element[0].nodeName;
-              var message = 'Function "'+boundFunc+'" called on '+type+' element'+id+' does not '+
-              'exist in that scope.';
-              console.groupCollapsed(message);
-              console.log(element[0]);
-              console.log(scope);
-              console.groupEnd();
-            }
-          }
-          element.on(name.substring(2).toLowerCase(), function(event) {
-            scope.$apply(function() {
-              fn(scope, {$event:event});
-            });
-          });
-        };
-      };
-      return $delegate;
-    })
-    var ngEventCheck = function() {
-      eeLib.getEventDirectives().forEach(function(name) {
-        eeLib.ngEventDirectives[name] = ['$parse', function($parse) {
-
-        }]
-      });
+    var ngEventDirs = eeLib.getEventDirectives();
+    for(var directive in ngEventDirs) {
+      var dirName = ngEventDirs[directive]+'Directive';
+      $provide.decorator(dirName, ['$delegate', '$timeout',
+        function($delegate, $timeout) {
+          var original = $delegate[0].compile;
+          var falseBinds = [];
+          var messages = [];
+          eeLib.delayDisplay(messages, $timeout);
+          $delegate[0].compile = function(element, attrs, transclude) {
+            return function ngEventHandler(scope, element, attrs) {
+              var ngEventDirs = eeLib.getEventDirectives(), boundFunc;
+              for(var attr in attrs.$attr) {
+                boundFunc = attrs[attr].substring(0,attrs[attr].indexOf('('));
+                if(ngEventDirs[attr] && !scope[boundFunc]) {
+                  messages.push({
+                    scope: scope,
+                    element:element,
+                    attrs: attrs,
+                    boundFunc: boundFunc
+                  });
+                }
+              }
+              element.on(name.substring(2).toLowerCase(), function(event) {
+                scope.$apply(function() {
+                  fn(scope, {$event:event});
+                });
+              });
+            };
+          };
+          return $delegate;
+      }]);
     }
   }]);
--- ee-lib/ee-lib.js
+++ ee-lib/ee-lib.js
@@ -1,5 +1,6 @@
 var eeLib = {
-  ngEventDirectives: {}
+  ngEventDirectives: {},
+  currentPromises: {}
 };
 eeLib.getEventDirectives = function() {
   var list = 'click dblclick mousedown mouseup mouseover mouseout mousemove mouseenter mouseleave keydown keyup keypress submit focus blur copy cut paste'.split(' ');
@@ -12,4 +13,26 @@ eeLib.getEventDirectives = function() {
 }
 eeLib.camelToDashes = function(str) {
  return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();});
+}
+eeLib.formatResults = function(messages) {
+  if(messages.length) {
+    console.groupCollapsed('Angular Hint: Events');
+    messages.forEach(function(obj) {
+      var id = (obj.element[0].id) ? ' with id: #'+obj.element[0].id : '';
+      var type = obj.element[0].nodeName;
+      var message = 'Function "'+obj.boundFunc+'" called on '+type+' element'+id+' does not '+
+      'exist in that scope.';
+      console.groupCollapsed(message);
+      console.log(obj.element[0]);
+      console.log(obj.scope);
+      console.groupEnd();
+    });
+    console.groupEnd();
+  }
+}
+eeLib.delayDisplay = function(messages, $timeout) {
+  $timeout.cancel(eeLib.currentPromises);
+  eeLib.currentPromises = $timeout(function() {
+    eeLib.formatResults(messages);
+  }.bind(this),250)
 }
