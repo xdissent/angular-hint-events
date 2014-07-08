60d9981e2692b7dd57abe321a751862ed83b9b1f
Carlo s A. Guillen <caguillen@google.com>
2014-07-08 12:54:35 -0700

    bug(ngHintEvent): fix unpatched compile function

--- decorator.js
+++ decorator.js
@@ -3,31 +3,33 @@ angular.module('ngHintEvent',[])
     var ngEventDirs = eeLib.getEventDirectives();
     for(var directive in ngEventDirs) {
       var dirName = ngEventDirs[directive]+'Directive';
-      $provide.decorator(dirName, ['$delegate', '$timeout',
-        function($delegate, $timeout) {
-          var original = $delegate[0].compile;
-          var falseBinds = [];
-          var messages = [];
+      $provide.decorator(dirName, ['$delegate', '$timeout', '$parse',
+        function($delegate, $timeout, $parse) {
+          var original = $delegate[0].compile, falseBinds = [], messages = [];
           eeLib.delayDisplay(messages, $timeout);
           $delegate[0].compile = function(element, attrs, transclude) {
+            var eventAttrName = eeLib.getEventAttr(attrs.$attr);
+            var fn = $parse(attrs[eventAttrName]);
             return function ngEventHandler(scope, element, attrs) {
               var ngEventDirs = eeLib.getEventDirectives(), boundFunc;
               for(var attr in attrs.$attr) {
-                boundFunc = attrs[attr].substring(0,attrs[attr].indexOf('('));
-                if(ngEventDirs[attr] && !scope[boundFunc]) {
-                  messages.push({
-                    scope: scope,
-                    element:element,
-                    attrs: attrs,
-                    boundFunc: boundFunc
-                  });
-                }
-              }
-              element.on(name.substring(2).toLowerCase(), function(event) {
-                scope.$apply(function() {
-                  fn(scope, {$event:event});
+                boundFuncs = eeLib.getFunctionNames(attrs[attr]);
+                boundFuncs.forEach(function(boundFn) {
+                  if(ngEventDirs[attr] && !scope[boundFn]) {
+                    messages.push({
+                      scope: scope,
+                      element:element,
+                      attrs: attrs,
+                      boundFunc: boundFn
+                    });
+                  }
                 });
+              }
+              element.on(eventAttrName.substring(2).toLowerCase(), function(event) {
+              scope.$apply(function() {
+                fn(scope, {$event:event});
               });
+            });
             };
           };
           return $delegate;
--- ee-lib/ee-lib.js
+++ ee-lib/ee-lib.js
@@ -9,11 +9,15 @@ eeLib.getEventDirectives = function() {
     var name = 'ng'+x.charAt(0).toUpperCase()+x.substring(1);
     eventDirHash[name]=name;
   });
+  eeLib.ngEventDirectives = eventDirHash;
   return eventDirHash;
-}
+};
 eeLib.camelToDashes = function(str) {
- return str.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();});
-}
+ return str.replace(/([A-Z])/g, function($1){return '-'+$1.toLowerCase();});
+};
+eeLib.dashesToCamel = function(str) {
+  return str.replace(/(-.)/g, function($1){return $1.charAt(1).toUpperCase();});
+};
 eeLib.formatResults = function(messages) {
   if(messages.length) {
     console.groupCollapsed('Angular Hint: Events');
@@ -29,10 +33,28 @@ eeLib.formatResults = function(messages) {
     });
     console.groupEnd();
   }
-}
+};
 eeLib.delayDisplay = function(messages, $timeout) {
   $timeout.cancel(eeLib.currentPromises);
   eeLib.currentPromises = $timeout(function() {
     eeLib.formatResults(messages);
   }.bind(this),250)
-}
+};
+eeLib.getFunctionNames = function(str) {
+  var results = str.split(/[\+\-\/\|\<\>\^=&!%~]/g).map(function(x){
+    if(isNaN(+x)) {
+      if(x.match(/\w+\(.*\)$/)){
+        return x.substring(0,x.indexOf('('));
+      }
+      return '';
+    }
+  }).filter(function(x){return x;});
+  return results;
+};
+eeLib.getEventAttr = function(attrs) {
+  for(var attr in attrs) {
+    if(eeLib.ngEventDirectives[attr]) {
+      return eeLib.dashesToCamel(attr);
+    }
+  }
+};
