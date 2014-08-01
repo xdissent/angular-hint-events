05eafe8aba45b028c6670853a57422142bf58f67
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-01 11:33:28 -0700

    style(demoApp): update demoApp style according to codeClimate linting

--- demo-app/app.js
+++ demo-app/app.js
@@ -12,7 +12,13 @@ angular.module('demoApp').controller('DemoController', ['$scope', function($scop
   $scope.name = 'Angular Hint: Events';
   $scope.count = 0;
   $scope.divideValue = 0;
-  $scope.increment = function(){++$scope.count};
-  $scope.dividend = function(){return Math.random()*20+20};
-  $scope.divisor = function(){return Math.random()*10+1};
+  $scope.increment = function() {
+    ++$scope.count;
+  };
+  $scope.dividend = function() {
+    return Math.random()*20+20;
+  };
+  $scope.divisor = function() {
+    return Math.random()*10+1;
+  };
 }]);
