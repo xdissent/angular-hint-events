70b2b6119e1e92cee0299e22569be67d950b90d1
Carlo s A. Guillen <caguillen@google.com>
2014-08-13 13:14:35 -0700

    chore(getEventDirectives_test): remove test for versions of angular

--- test/getEventDirectives_test.js
+++ test/getEventDirectives_test.js
@@ -6,36 +6,4 @@ describe('getEventDirectives()', function() {
     expect(dirs['ngClick']).toBe('ngClick');
     expect(dirs['ngMouseover']).toBe('ngMouseover');
   });
-
-  it('should return a list specific to the angular version', function() {
-    var dirs;
-
-    angular.version.minor = 0;
-    angular.version.dot = 0;
-    dirs = getEventDirectives();
-    expect(dirs['ngKeyup']).toBeUndefined();
-
-    angular.version.minor = 1;
-    angular.version.dot = 2;
-    dirs = getEventDirectives();
-    expect(dirs['ngKeypress']).toBeUndefined();
-
-    angular.version.minor = 1;
-    angular.version.dot = 4;
-    dirs = getEventDirectives();
-    expect(dirs['ngBlur']).toBeUndefined();
-
-    angular.version.minor = 2;
-    angular.version.dot = 0;
-    dirs = getEventDirectives();
-    expect(dirs['ngCut']).toBeDefined();
-
-    angular.version.minor = 1;
-    angular.version.dot = 3;
-    expect(dirs['ngPaste']).toBeDefined();
-
-    angular.version.minor = 3;
-    angular.version.dot = 2;
-    expect(dirs['ngPaste']).toBeDefined();
-  });
 });
